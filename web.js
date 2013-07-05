708f20ec5701d163262dbf861fa00aceeed04ac8
Ubuntu <ubuntu@ip-172-31-35-76.us-west-2.compute.internal>
2013-07-05 19:05:18 +0000

    Testing, part two

--- web.js
+++ web.js
@@ -3,12 +3,9 @@ var express = require('express'), fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send("foo");
-    /*
     fs.readFile("index.html", 'utf8', function(err, data) {
 	response.send(data);
     });
-    */
 });
 
 var port = process.env.PORT || 5000;
