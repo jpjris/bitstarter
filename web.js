64fbf705816af3b94ff380a3bad5e32edeee0bfb
Ubuntu <ubuntu@ip-172-31-35-76.us-west-2.compute.internal>
2013-07-05 18:58:32 +0000

    Testing

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require('express'), fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send("foo");
+    /*
+    fs.readFile("index.html", 'utf8', function(err, data) {
+	response.send(data);
+    });
+    */
 });
 
 var port = process.env.PORT || 5000;
